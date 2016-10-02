16a2c92934972f6c73abe5136f304d398213d5bc
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-10-02 15:54:10 +0200

    Get dates from website

--- Timetable/src/com/Kitowski/timetable/Timetable.java
+++ Timetable/src/com/Kitowski/timetable/Timetable.java
@@ -1,13 +1,18 @@
 package com.Kitowski.timetable;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 public class Timetable extends Activity {
 	private LinearLayout layout;
 	private ProgressBar loadBar;
+	
+	private ArrayList<String> dates;
 	
 	
 	@Override
@@ -18,5 +23,17 @@ public class Timetable extends Activity {
 		layout = (LinearLayout)findViewById(R.id.mainLayout);
 		loadBar = new ProgressBar(this);
 		layout.addView(loadBar);
+		
+		HttpReader http = (HttpReader) new HttpReader().execute("https://inf.ug.edu.pl/terminy-zjazdow-semestr-zimowy-201617", "table");
+		
+		
+		try {
+			dates = http.get();
+			
+			for(String str : dates) Log.i("TEST", str);
+		}
+		catch(Exception e) {
+			
+		}
 	}
 }
