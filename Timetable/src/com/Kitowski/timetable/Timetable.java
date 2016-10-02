bf1d9848ccbf030c5abffdeb4846e4d69c6083ea
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-10-02 14:59:48 +0200

    Added progressBar to activity

--- Timetable/src/com/Kitowski/timetable/Timetable.java
+++ Timetable/src/com/Kitowski/timetable/Timetable.java
@@ -2,11 +2,21 @@ package com.Kitowski.timetable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 public class Timetable extends Activity {
+	private LinearLayout layout;
+	private ProgressBar loadBar;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timetable);
+		
+		layout = (LinearLayout)findViewById(R.id.mainLayout);
+		loadBar = new ProgressBar(this);
+		layout.addView(loadBar);
 	}
 }
