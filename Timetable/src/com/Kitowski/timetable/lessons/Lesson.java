de27f9255ba389c974fb9baf628b68d5da180970
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-10-03 11:29:47 +0200

    Display lesson details

--- Timetable/src/com/Kitowski/timetable/lessons/Lesson.java
+++ Timetable/src/com/Kitowski/timetable/lessons/Lesson.java
@@ -3,17 +3,21 @@ package com.Kitowski.timetable.lessons;
 import com.Kitowski.timetable.Timetable;
 
 import android.R.color;
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.view.View;
 import android.widget.TextView;
 
 public class Lesson extends TextView {
 	private String lessonData[];
 	private GradientDrawable background;
+	private Timetable timetable;
 	
 	@SuppressWarnings("deprecation")
 	public Lesson(Timetable timetable, String lesson) {
 		super(timetable);
+		this.timetable = timetable;
 		
 		lessonData = lesson.split(",");
 		background = new GradientDrawable();
@@ -23,5 +27,29 @@ public class Lesson extends TextView {
 		this.setText(lessonData[0] + " " + lessonData[1]);
 		this.setPadding(15, 15, 15, 15);
 		this.setBackgroundDrawable(background);
+		
+		this.setOnClickListener(new OnClickListener() {
+			@Override public void onClick(View v) { displayDetails(); }
+        });
+	}
+	
+	private void displayDetails() {
+		AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(timetable);
+		dlgAlert.setTitle(lessonData[1]);
+		dlgAlert.setMessage(formatDetails());
+		dlgAlert.setPositiveButton("OK", null);
+		dlgAlert.setCancelable(true);
+		dlgAlert.create().show();
+	}
+	
+	private String formatDetails() {
+		String buffer = lessonData[2] + "\n";
+		
+		for(int i = 3; i < lessonData.length; ++i) {
+			if(lessonData[i].contains("gr.")) buffer += " - " + lessonData[i]; //group
+			else if(lessonData[i].replaceAll("\\s", "").matches("-?\\d+(\\.\\d+)?")) buffer += ", sala: " + lessonData[i] + "\n"; //room
+			else buffer += lessonData[i];
+		}
+		return buffer;
 	}
 }
