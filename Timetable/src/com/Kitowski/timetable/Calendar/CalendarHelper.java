0396c5642355a747be6845c148347fbfd8f48e08
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-10-03 20:54:25 +0200

    Minor change

--- Timetable/src/com/Kitowski/timetable/Calendar/CalendarHelper.java
+++ Timetable/src/com/Kitowski/timetable/Calendar/CalendarHelper.java
@@ -12,19 +12,23 @@ import com.Kitowski.timetable.lessons.Lesson;
 import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
 import android.provider.CalendarContract.Reminders;
 import android.provider.ContactsContract.CommonDataKinds.Event;
+import android.util.Log;
 
 @SuppressLint("SimpleDateFormat")
 @SuppressWarnings("unused")
 public class CalendarHelper {
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	
 	public static boolean addToCalendar(Timetable timetable, String date, Lesson lesson, boolean withAlarm) {
 		try {
-			Date start = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(date + " " + lesson.getRawTime()[0]);
-			Date end = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(date + " " + lesson.getRawTime()[1]);
+			Date start = dateFormat.parse(date + " " + lesson.getRawTime()[0]);
+			Date end = dateFormat.parse(date + " " + lesson.getRawTime()[1]);
 			Calendar beginTime = Calendar.getInstance();
 			Calendar endTime = Calendar.getInstance();
 			beginTime.setTime(start);
