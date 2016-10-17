4ebfa7b11fb5112f2ce74b34c4b65952bc0ba0ac
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-10-17 13:17:25 +0200

    Load selectYear state from shared preferences + disable spinner if selectYear is false

--- Timetable/src/com/Kitowski/Settings/Settings.java
+++ Timetable/src/com/Kitowski/Settings/Settings.java
@@ -10,22 +10,25 @@ import android.widget.CheckBox;
 import android.widget.Spinner;
 
 public class Settings extends Activity {
-	private CheckBox legend;
+	private CheckBox displayLegend, selectYear;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		
-		legend = (CheckBox)findViewById(R.id.checkbox_displaylegend);
+		displayLegend = (CheckBox)findViewById(R.id.checkbox_displaylegend);
+		selectYear = (CheckBox)findViewById(R.id.checkbox_displayyear);
 		
-		load();
+		loadCheckboxes();
 		updateSelectYearSpinnerValues();
 	}
 	
-	private void load() {
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);		
-		legend.setChecked(preferences.getBoolean("Legend", true));
+	private void loadCheckboxes() {
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		displayLegend.setChecked(pref.getBoolean("Legend", true));
+		selectYear.setChecked(pref.getBoolean("SelectYear", false));
 	}
 	
 	private void updateSelectYearSpinnerValues() {
@@ -34,5 +37,6 @@ public class Settings extends Activity {
 		spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinnerArrayAdapter.addAll(getIntent().getStringArrayExtra("groups"));
 		spn.setAdapter(spinnerArrayAdapter);
+		if(!selectYear.isChecked()) spn.setEnabled(false);
 	}
 }
