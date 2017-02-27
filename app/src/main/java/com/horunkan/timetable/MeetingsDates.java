99581a74bf4417be30a0601ea55e9dd873979df4
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-02-27 19:48:47 +0100

    Convert date to needed format

--- app/src/main/java/com/horunkan/timetable/MeetingsDates.java
+++ app/src/main/java/com/horunkan/timetable/MeetingsDates.java
@@ -14,13 +14,19 @@ public class MeetingsDates {
     private static final String logcat = "MeetingsDates";
     private static final String logcatVal = logcat + "-value";
 
+    private final String dateFormat = "%s-%02d-%02d";
+    private final String monthYearRegex = "^[A-Za-ż]+ [0-9]{4}$";
+    private final String dayRegex = "^[0-9]+/*[0-9]*$";
+
+    final String[] months = {"styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"};
+
     private final String[] meetingTable;
     private ArrayList<String> dates;
 
     public MeetingsDates(Context con) {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(con);
         meetingTable = new String[2];
-        dates = new ArrayList<String>();
+        dates = new ArrayList<>();
 
         meetingTable[0] = pref.getString("MeetingTableWinter", "https://inf.ug.edu.pl/terminy-zjazdow-semestr-zimowy-201617.print");
         meetingTable[1] = pref.getString("MeetingTableSummer", "https://inf.ug.edu.pl/terminy-zjazdow-sem-letni-2016-17.print");
@@ -34,6 +40,7 @@ public class MeetingsDates {
         try {
             ArrayList<String> toConvert = HtmlFormatter.getCleanWebsite(http.get());
             toConvert = getDateContentOnly(toConvert);
+            formatDates(toConvert);
         }
         catch (Exception e) {
             Log.e(logcat, e.getMessage());
@@ -41,9 +48,7 @@ public class MeetingsDates {
     }
 
     private ArrayList<String> getDateContentOnly(ArrayList<String> arr) {
-        final String monthYearRegex = "^[A-Za-ż]+ [0-9]{4}$";
-        final String dayRegex = "^[0-9]+/*[0-9]*$";
-        ArrayList<String> buffer = new ArrayList<String>();
+        ArrayList<String> buffer = new ArrayList<>();
 
         Log.i(logcat, "Start array filtering");
         for(String line : arr) {
@@ -55,5 +60,37 @@ public class MeetingsDates {
         Log.i(logcat, String.format("Array filtering finished\nLines left: %d\nLines removed: %d", buffer.size(), arr.size() - buffer.size()));
 
         return buffer;
+    }
+
+    private void formatDates(ArrayList<String> arr) {
+        String year = "";
+        int month = 0;
+
+        Log.i(logcat, "Start dates convert");
+        for(String str : arr) {
+            if(str.matches(monthYearRegex)) {
+                year = getYear(str);
+                month = getMonth(str);
+            }
+            else addDate(year, month, str);
+        }
+
+        for(String str : dates) Log.i(logcatVal, str);
+        Log.i(logcat, String.format("Finished dates convert\nDates count: %d", dates.size()));
+    }
+
+    private String getYear(String str) { return str.split(" ")[1]; }
+
+    private int getMonth(String str) {
+        String buffer = str.split(" ")[0];
+        for(int i = 0; i < 12; ++i) if(months[i].contentEquals(buffer)) return i + 1;
+        return 1;
+    }
+
+    private void addDate(String year, int month, String days) {
+        String[] day = days.split("/");
+
+        dates.add(String.format(dateFormat, year, month, Integer.parseInt(day[0])));
+        if(day.length > 1) dates.add(String.format(dateFormat, year, month, Integer.parseInt(day[1])));
     }
 }
