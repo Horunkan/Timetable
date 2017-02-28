7ad0b67c8320db73312a30cab0a9271dc3425ef0
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-02-28 16:05:28 +0100

    Added DateParser + set closest date on DateSpinner

--- app/src/main/java/com/horunkan/timetable/DateSpinner.java
+++ app/src/main/java/com/horunkan/timetable/DateSpinner.java
@@ -6,7 +6,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.horunkan.timetable.utilities.DateParser;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class DateSpinner implements AdapterView.OnItemSelectedListener {
     private static final String logcat = "DateSpinner";
@@ -22,14 +25,30 @@ public class DateSpinner implements AdapterView.OnItemSelectedListener {
 
         spinner.setAdapter(spinnerArrayAdapter);
         spinner.setOnItemSelectedListener(this);
-        spinner.setSelection(0);
+
+        updateSelection(dates);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Log.i(logcat, String.format("Selected: %s", spinner.getSelectedItem()));
+        Log.i(logcat, String.format("Selected date: %s", spinner.getSelectedItem()));
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) { }
+
+    private void updateSelection(ArrayList<String> dates) {
+        Date currentDate = DateParser.parse(new Date(), "00:00");
+
+
+        for(int i = 0; i < dates.size(); ++i) {
+            Date buffer = DateParser.parse(dates.get(i), "00:00");
+
+            if(currentDate.before(buffer) || currentDate.compareTo(buffer) == 0) {
+                Log.i(logcat, String.format("Closest date is: %s", dates.get(i)));
+                spinner.setSelection(i);
+                break;
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/horunkan/timetable/utilities/DateParser.java
@@ -0,0 +1,26 @@
+package com.horunkan.timetable.utilities;
+
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParser {
+    private static final String logcat = "DateParser";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+    public static Date parse(String date, String time) {
+        try {
+            Date buffer = dateFormat.parse(date + " " + time);
+            return buffer;
+        }
+        catch (Exception e) {
+            Log.e(logcat, String.format("Failed to parse: %s, %s - $s", date, time, e.getMessage()));
+            return null;
+        }
+    }
+
+    public static Date parse(Date date, String time) {
+        return parse(new SimpleDateFormat("yyyy-MM-dd").format(date), time);
+    }
+}
