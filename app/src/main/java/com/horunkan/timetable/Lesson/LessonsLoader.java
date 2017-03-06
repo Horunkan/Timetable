3ac6e16fde938db0c0941638ea1044e119b658f9
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-03-06 11:50:06 +0100

    Fixed lessons assign to groups

--- app/src/main/java/com/horunkan/timetable/Lesson/LessonsLoader.java
+++ app/src/main/java/com/horunkan/timetable/Lesson/LessonsLoader.java
@@ -28,8 +28,8 @@ public class LessonsLoader {
         HttpLoader http = (HttpLoader) new HttpLoader().execute(String.format("https://inf.ug.edu.pl/plan-%s.print", date));
 
         try {
-            ArrayList<String> toConvert = http.get();
-            toConvert = getLessonsContentOnly(toConvert);
+            ArrayList<String> toConvert = HtmlFormatter.getCleanWebsite(http.get());
+            for(String str: toConvert) Log.i(logcatVal, str);
             formatLessons(toConvert);
         }
         catch (Exception e) {
@@ -37,46 +37,28 @@ public class LessonsLoader {
         }
     }
 
-    private ArrayList<String> getLessonsContentOnly(ArrayList<String> arr) {
-        ArrayList<String> buffer = new ArrayList<>();
-        Boolean lessonContent = false;
-
-        Log.i(logcat, "Start array filtering");
-        for(String line : arr) {
-            if(line.matches("<ul>")) lessonContent = true;
-            if(lessonContent) buffer.add(line);
-            if(line.matches("</ul>")) lessonContent = false;
-        }
-
-        for(String str : buffer) Log.i(logcatVal, str);
-        Log.i(logcat, String.format("Array filtering finished\nLines left: %d\nLines removed: %d", buffer.size(), arr.size() - buffer.size()));
-
-        return buffer;
-    }
-
     private void formatLessons(ArrayList<String> arr) {
         ArrayList<String> buffer = new ArrayList<>();
         int index = 0;
 
         for(String line : arr) {
-            if(line.matches("</ul>") && index < 5) {
+            if(line.contains("—") || line.contains("brak")) buffer.add(line);
+            else if(line.contains("Studia") && buffer.size() > 0) {
                 assignLessonsToGroups(buffer, index);
                 ++index;
                 buffer = new ArrayList<>();
             }
-            else buffer.add(line);
+            if(index >= 5) break;
         }
+
+        assignLessonsToGroups(buffer, index); //Last group
     }
 
     private void assignLessonsToGroups(ArrayList<String> arr, int index) {
         Log.i(logcat, String.format("Start assign lessons to index: %d", index));
-
         lessons[index] = new ArrayList<>();
 
-        arr = HtmlFormatter.getCleanWebsite(arr);
-
         for(String str : arr) {
-            //Log.i(logcatVal, str);
             if(!str.contains("zajęcia zostały")) lessons[index].add(new Lesson(activity, str)); //TODO Canceled lessons generate bugs with lessons loading
             Log.i(logcatVal, str);
         }
