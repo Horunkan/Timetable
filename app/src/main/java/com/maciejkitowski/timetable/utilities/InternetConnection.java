6f0d5e4ab5704b2fcec89a484291033d13be0c25
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-11-03 16:12:11 +0100

    Check internet connection

---
+++ app/src/main/java/com/maciejkitowski/timetable/utilities/InternetConnection.java
@@ -0,0 +1,24 @@
+package com.maciejkitowski.timetable.utilities;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public final class InternetConnection {
+    private static final String logcat = "InternetConnection";
+
+    private InternetConnection() {}
+
+    public static boolean isConnected(Context context) {
+        Log.i(logcat, "Check internet connection");
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        boolean connected = netInfo != null && netInfo.isAvailable() && netInfo.isConnected();
+
+        Log.i(logcat, String.format("Internet connected: %s", connected));
+        return connected;
+    }
+}
