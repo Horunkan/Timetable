42d31fa9eef51d1e8a1d9891d6bd23e92b3024c0
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-11-07 13:04:05 +0100

    Move loading bar toggle and alert text display to MainActivity (as static)

--- app/src/main/java/com/maciejkitowski/timetable/Date/HtmlLoader.java
+++ app/src/main/java/com/maciejkitowski/timetable/Date/HtmlLoader.java
@@ -3,12 +3,11 @@ package com.maciejkitowski.timetable.Date;
 import android.content.Context;
 import android.util.Log;
 
+import com.maciejkitowski.timetable.MainActivity;
 import com.maciejkitowski.timetable.R;
-import com.maciejkitowski.timetable.utilities.AlertText;
 import com.maciejkitowski.timetable.utilities.DataReceivedListener;
 import com.maciejkitowski.timetable.utilities.HtmlDownloader;
 import com.maciejkitowski.timetable.utilities.InternetConnection;
-import com.maciejkitowski.timetable.utilities.LoadingBarToggle;
 
 import java.util.List;
 
@@ -18,15 +17,11 @@ final class HtmlLoader implements ILoader, DataReceivedListener {
 
     private Context context;
     private Loader loader;
-    private AlertText alert;
-    private LoadingBarToggle loadingBar;
     List<String> downloaded;
 
     public HtmlLoader(Context context, Loader loader) {
         this.context = context;
         this.loader = loader;
-        loadingBar = new LoadingBarToggle(context);
-        alert = new AlertText(context);
     }
 
     @Override
@@ -38,14 +33,14 @@ final class HtmlLoader implements ILoader, DataReceivedListener {
     @Override
     public void onDataReceivedBegin() {
         Log.i(logcat, "Download started");
-        loadingBar.display();
+        MainActivity.loadingBar.display();
     }
 
     @Override
     public void onDataReceivedSuccess(List<String> data) {
         Log.i(logcat, "Download success");
         for(String str : data) Log.i(logcat + "-val", str);
-        loadingBar.hide();
+        MainActivity.loadingBar.hide();
         downloaded = data;
         loader.receivedJson(downloaded);
     }
@@ -53,13 +48,13 @@ final class HtmlLoader implements ILoader, DataReceivedListener {
     @Override
     public void onDataReceivedFailed() {
         Log.w(logcat, "Download failed without exception");
-        loadingBar.hide();
+        MainActivity.loadingBar.hide();
     }
 
     @Override
     public void onDataReceivedFailed(Exception ex) {
         Log.w(logcat, String.format("Download failed with exception: %s", ex.getLocalizedMessage()));
-        loadingBar.hide();
+        MainActivity.loadingBar.hide();
     }
 
     private void startDownloading() {
@@ -78,6 +73,6 @@ final class HtmlLoader implements ILoader, DataReceivedListener {
 
     private void displayNoConnectionError() {
         Log.i(logcat, "Internet not connected, display error.");
-        alert.display(R.string.error_nointernet);
+        MainActivity.alertDisplayer.display(R.string.error_nointernet);
     }
 }
--- app/src/main/java/com/maciejkitowski/timetable/MainActivity.java
+++ app/src/main/java/com/maciejkitowski/timetable/MainActivity.java
@@ -2,17 +2,30 @@ package com.maciejkitowski.timetable;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.maciejkitowski.timetable.Date.Loader;
+import com.maciejkitowski.timetable.utilities.AlertText;
+import com.maciejkitowski.timetable.utilities.LoadingBarToggle;
 
 public class MainActivity extends AppCompatActivity {
-    private Loader dateLoader;
+    public static AlertText alertDisplayer;
+    public static LoadingBarToggle loadingBar;
+    private static final String logcat = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dateLoader = new Loader(this);
+        initialize();
+    }
+
+    private void initialize() {
+        Log.i(logcat, "Initialize");
+        alertDisplayer = new AlertText(this);
+        loadingBar = new LoadingBarToggle(this);
+
+        Loader dateLoader = new Loader(this);
     }
 }
