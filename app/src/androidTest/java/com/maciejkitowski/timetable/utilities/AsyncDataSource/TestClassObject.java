02b46771390a9390fdc572f74d2a8d7e16c529fd
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-11-21 11:15:35 +0100

    Minor fixes

--- app/src/androidTest/java/com/maciejkitowski/timetable/utilities/AsyncDataSource/TestClassObject.java
+++ app/src/androidTest/java/com/maciejkitowski/timetable/utilities/AsyncDataSource/TestClassObject.java
@@ -2,28 +2,26 @@ package com.maciejkitowski.timetable.utilities.AsyncDataSource;
 
 import android.util.Log;
 
-import com.maciejkitowski.timetable.utilities.AsyncDataListener;
-
 import java.util.List;
 
-final class TestClassObject implements AsyncDataListener {
+final class TestClassObject implements AsyncDataSourceListener {
     private static final String logcat = "UnitTest";
     boolean fail = false;
     boolean success = false;
-
+    
     @Override
-    public void onReceiveBegin() {
+    public void onLoadBegin() {
         Log.i(logcat, "Receive begin");
     }
 
     @Override
-    public void onReceiveSuccess(List<String> data) {
+    public void onLoadSuccess(List<String> data) {
         Log.i(logcat, "Receive success");
         success = true;
     }
 
     @Override
-    public void onReceiveFail(String message) {
+    public void onLoadFail(String message) {
         Log.i(logcat, "Receive fail: " + message);
         fail = true;
     }
